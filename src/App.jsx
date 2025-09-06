cf1f11ee66c0ee5d0d6b6521d5d34bdb91110207
Ujawal karki <ujawalkarki777@gmail.com>
2025-09-07 00:35:59 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Login from './pages/Login'
+import Feed from './pages/Feed'
+import Messages from './pages/Messages'
+import ChatBox from './pages/ChatBox'
+import Connections from './pages/Connections'
+import Discover from './pages/Discover'
+import Profile from './pages/Profile'
+import CreatePost from './pages/CreatePost'
+import {useUser, useAuth} from '@clerk/clerk-react'
+import Layout from './pages/Layout'
+import {Toaster} from 'react-hot-toast'
+import { useEffect } from 'react'
+
+
+const App = () => {
+  const {user} = useUser()
+  const{getToken} = useAuth()
+
+  useEffect(()=>{
+       if(user){
+        getToken().then((token)=>console.log(token))
+       }
+  },[user])
+  return (
+    <>
+    <Toaster />
+      <Routes>
+         <Route path='/' element={ !user ? <Login/> : <Layout/>}>
+         <Route index element={<Feed/>}/>
+         <Route path='messages' element={<Messages/>}/>
+         <Route path='messages/:userId' element={<ChatBox/>}/>
+         <Route path='Connections' element={<Connections/>}/>
+         <Route path='discover' element={<Discover/>}/>
+         <Route path='profile' element={<Profile/>}/>
+         <Route path='profile/:profileID' element={<Profile/>}/>
+         <Route path='create-post' element={<CreatePost/>}/>
+         </Route>
+      </Routes>
+    </>
+  )
+}
+
+export default App
